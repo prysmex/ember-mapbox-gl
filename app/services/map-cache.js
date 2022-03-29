ec6c9eae64789a8cd7e4ac66cf6d124de1cccb03
Gustavo Rodriguez <gustavo@bengala.tech>
2022-03-29 10:41:49 -0500

    Add map cache service for caching map instances for quick rerenders

---
+++ app/services/map-cache.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-mapbox-gl/services/map-cache';
---
+++ tests/unit/services/map-cache-test.js
@@ -0,0 +1,12 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | map-cache', function (hooks) {
+  setupTest(hooks);
+
+  // Replace this with your real tests.
+  test('it exists', function (assert) {
+    let service = this.owner.lookup('service:map-cache');
+    assert.ok(service);
+  });
+});
