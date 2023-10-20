d576b9ea04cf26499a86835feba0873450df0727
Gustavo Rodriguez <gustavo@bengala.tech>
2023-10-20 14:12:56 -0500

    fix: Transfer ownership and update dependencies

--- tests/integration/helpers/mapbox-gl-on-test.js
+++ tests/integration/helpers/mapbox-gl-on-test.js
@@ -1,7 +1,5 @@
-import { module, test } from 'qunit';
+import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-// import { render } from '@ember/test-helpers';
-// import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Helper | mapbox-gl-on', function (hooks) {
   setupRenderingTest(hooks);
